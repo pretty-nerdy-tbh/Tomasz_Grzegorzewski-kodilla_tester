267cc695450d2fa84ed64fc105dadca9a7433768
Tomasz Grzegorzewski <grzegorzewski.tomasz9@gmail.com>
2024-10-29 21:07:32 +0100

    moduł 0.7

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Tomek");
+        System.out.println("25");
+        System.out.println("G");
+
+    }
+}
