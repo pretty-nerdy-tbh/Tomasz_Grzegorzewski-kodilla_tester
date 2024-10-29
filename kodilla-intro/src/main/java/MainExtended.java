b1a8fd0ee76015a59dc29850f0871ec5a3e4d981
Tomasz Grzegorzewski <grzegorzewski.tomasz9@gmail.com>
2024-10-29 21:59:24 +0100

    moduł 0.8

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "Tomasz";
+        String surname = "Grzeorzewski";
+        int yearBorn = 1999;
+        double hight = 187.5;
+        char bloodType = 'A';
+        boolean isReadyToLearn = true;
+        System.out.println(name + " " + surname);
+        System.out.println("Born in " + yearBorn);
+        System.out.println("Measures: " + hight + " cm");
+        System.out.println("Blood type: " + bloodType + "+");
+        System.out.println("Everything above supposed to be " + isReadyToLearn);
+    }
+}
