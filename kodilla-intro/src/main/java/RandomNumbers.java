c20998b7b3e336418bdbf86470c857c1014248c7
Tomasz Grzegorzewski <grzegorzewski.tomasz9@gmail.com>
2024-11-17 17:47:06 +0100

    Moduł 2.5

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,31 +1,32 @@
 import java.util.Random;
 
 public class RandomNumbers {
+    public int temp;
 
-    public int getRandomNumbers() {
+    public void getRandomNumbers() {
         Random random = new Random();
         int max = 5000;
         int sum = 0;
-        int temp = 0;
+        this.temp = 0;
         while (sum <= max) {
-            temp = random.nextInt(30);
-            sum = sum + temp;
+            this.temp = random.nextInt(30);
+            sum = sum + this.temp;
         }
+    }
 
-        public int lowestNumber() {
-            int lowestNumber = 5000;
-            while (lowestNumber > temp) {
-                lowestNumber = temp;
-            }
-            return lowestNumber;
+    public int getLowestNumber() {
+        int lowestNumber = 5000;
+        while (lowestNumber > this.temp) {
+            lowestNumber = this.temp;
         }
+        return lowestNumber;
+    }
 
-        public int highestNumber() {
-            int highestNumber = 0;
-            while (highestNumber < temp) {
-                highestNumber = temp;
-            }
-            return highestNumber;
+    public int gwtHighestNumber() {
+        int highestNumber = 0;
+        while (highestNumber < this.temp) {
+            highestNumber = this.temp;
         }
+        return highestNumber;
     }
 }
