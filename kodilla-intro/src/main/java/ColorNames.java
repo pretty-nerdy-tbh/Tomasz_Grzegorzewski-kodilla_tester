5b0621f53518c37833565e5d255b75b8902c483b
Tomasz Grzegorzewski <grzegorzewski.tomasz9@gmail.com>
2024-11-17 19:53:34 +0100

    Moduł 2.8

---
+++ kodilla-intro/src/main/java/ColorNames.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class ColorNames {
+
+    public static String getColorFromLetter() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter first letter of a color:");
+            String firstLetter = scanner.nextLine().trim().toUpperCase();
+            switch (firstLetter) {
+                case "A": return "Amber";
+                case "B": return "Black";
+                case "C": return "Charcoal";
+                case "D": return "Denim";
+                case "E": return "Ecru";
+                case "F": return "Fuchsia";
+                case "G": return "Gold";
+                case "H": return "Hazel";
+                case "I": return "Indigo";
+                case "J": return "Jade";
+                case "K": return "Khaki";
+                case "L": return "Lilac";
+                case "M": return "Magenta";
+                case "N": return "Navy";
+                case "O": return "Orange";
+                case "P": return "Pink";
+                case "Q": return "Quartz";
+                case "R": return "Red";
+                case "S": return "Scarlet";
+                case "T": return "Turquoise";
+                case "U": return "Ultramarine";
+                case "V": return "Violet";
+                case "W": return "White";
+                case "X": return "Xanthic";
+                case "Y": return "Yellow";
+                case "Z": return "Zomp";
+                default:
+                    System.out.println("I don't know this letter. Try different one.");
+            }
+        }
+    }
+}
---
+++ kodilla-intro/src/main/java/ColorNamesApplication.java
@@ -0,0 +1,7 @@
+public class ColorNamesApplication {
+    public static void main(String[] args) {
+        ColorNames color = new ColorNames();
+        String result = color.getColorFromLetter();
+        System.out.println("Result: " + result);
+    }
+}
