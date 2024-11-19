e50b2fddfaa97fd9adf8c669578158081c0ca0f1
Tomasz Grzegorzewski <grzegorzewski.tomasz9@gmail.com>
2024-11-19 20:22:12 +0100

    Moduł 2.6

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Application {
+    private String name;
+    private int age;
+    private double height;
+
+    public Application(String name, int age, double height){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public void checkAgeAndHeight (){
+        if (this.name != null) {
+            if (this.age > 30 && this.height > 160) {
+                System.out.println("User is older than 30 and taller than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+    }
 }
