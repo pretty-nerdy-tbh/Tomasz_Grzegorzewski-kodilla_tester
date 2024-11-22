1f8710237e544f25fd64e99c1951ad767b6add82
Tomasz Grzegorzewski <grzegorzewski.tomasz9@gmail.com>
2024-11-22 20:52:21 +0100

    Moduł 3.5

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -0,0 +1,33 @@
+package com.kodilla.basic_assertion;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        boolean correct = ResultChecker.assertEquals(13, sumResult);
+        if (correct) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        int subtractResult = calculator.subtract(a, b);
+        boolean correct1 = ResultChecker.assertEquals(-3, subtractResult);
+        if (correct1) {
+            System.out.println("Metoda subtract działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        int squaredResult = calculator.squared(a);
+        boolean correct2 = ResultChecker.assertEquals(25, squaredResult);
+        if (correct2) {
+            System.out.println("Metoda squared działa poprawnie dla liczby " + a);
+        } else {
+            System.out.println("Metoda squared nie działa poprawnie dla liczby " + a);
+        }
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,16 @@
+package com.kodilla.basic_assertion;
+
+public class Calculator {
+
+    public int sum(int a, int b){
+        return a + b;
+    }
+
+    public int subtract(int a, int b){
+        return a - b;
+    }
+
+    public int squared(int a){
+        return a * a;
+    }
+}
