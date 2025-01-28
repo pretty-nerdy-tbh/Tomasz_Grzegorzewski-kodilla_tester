c7b28c6c5a237c3fbf4c0dacce9fbeb9b62767bd
Tomasz Grzegorzewski <grzegorzewski.tomasz9@gmail.com>
2025-01-28 21:32:00 +0100

    Moduł 10.3

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -1,11 +1,13 @@
 package com.kodilla.spring.basic.spring_dependency_injection;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleApplication {
 
+    @Autowired
     @Qualifier(value = "skypeMessageService")
     private MessageService messageService;
 
