a4e94578c5036147284dae436d651e00206fbbd8
Tomasz Grzegorzewski <grzegorzewski.tomasz9@gmail.com>
2025-03-04 22:33:47 +0100

    Moduł 13.6

--- kodilla-login-selenium/src/test/java/com/kodilla/selenium/pom/StoreSearchPomTest.java
+++ kodilla-login-selenium/src/test/java/com/kodilla/selenium/pom/StoreSearchPomTest.java
@@ -3,6 +3,8 @@ package com.kodilla.selenium.pom;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -23,27 +25,29 @@ class StoreSearchPomTest {
         searchPom = new StoreSearchPom(driver);
     }
 
-    @Test
-    public void testSearch_CheckPositiveValidation() {
+    @ParameterizedTest
+    @CsvSource(value = {"2,NoteBook", "1,School", "1,Brand", "1,Bussines", "1,Gaming", "0,Powerful"})
+    public void testSearch_CheckPositiveValidation(int searchCount, String searchPhrase) {
         searchPom.cookiePopUpClicker();
-        searchPom.searchByPhrase("Powerful");
+        searchPom.searchByPhrase(searchPhrase);
         int count = searchPom.searchResultsCounter();
-        assertEquals(0, count);
+        assertEquals(searchCount, count);
     }
 
-    @Test
-    public void testNoCaseSensitivity_CheckPositiveValidation() {
+    @ParameterizedTest
+    @CsvSource(value = {"notebook,NoteBook", "school,School", "brand,Brand", "bussines,Bussines", "gaming,Gaming", "powerful,Powerful"})
+    public void testNoCaseSensitivity_CheckPositiveValidation(String lowerCasePhrase, String upperCasePhrase) {
         searchPom.cookiePopUpClicker();
 
-        searchPom.searchByPhrase("notebook");
-        int lowerCase = searchPom.searchResultsCounter();
+        searchPom.searchByPhrase(lowerCasePhrase);
+        int lowerCaseCount = searchPom.searchResultsCounter();
 
         searchPom.clearSearchBox();
 
-        searchPom.searchByPhrase("NoteBook");
-        int upperCase = searchPom.searchResultsCounter();
+        searchPom.searchByPhrase(upperCasePhrase);
+        int upperCaseCount = searchPom.searchResultsCounter();
 
-        assertEquals(lowerCase, upperCase);
+        assertEquals(lowerCaseCount, upperCaseCount);
     }
 
     @AfterEach
